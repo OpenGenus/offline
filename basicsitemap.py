24f7e44fab384871cfd56627ab90a9321277dcfe
Tanmay Maheshwari <tanmaymaheshwari1996@gmail.com>
2018-05-23 17:08:26 +0530

    Update basicsitemap.py
    I've changed the method to obtain domain name and check if the link belongs to same domain. I've tried changing the parser for html page but they all are giving same output. The problem with links like "https://discourse.opengenus.org/" is that, the parsed html page gives the links without domain name. I listed the links and they begin like "c/abc/xyz".  I'm searching if I can use some other library like BS4. 
    The error with "https://github.com/" has been resolved.

--- basicsitemap.py
+++ basicsitemap.py
@@ -1,42 +1,33 @@
-import urllib.request as Ureq
-import requests
+from urllib.request import urlopen
 from bs4 import BeautifulSoup as bs
-from urllib.parse import urlparse
+import tldextract
 def outputprint(url_in):
 	file = open('output.txt','a')
 	file.write('\n'+url_in)
 	file.close()
 def obtaindomain(url_inn):
-	url_p=urlparse(url_inn)
-	domainout='{uri.scheme}://{uri.netloc}/'.format(uri=url_p)
+	list =tldextract.extract(url_inn)
+	domainout=list.domain+'.'+list.suffix
 	return domainout
 def task(url,domain,allurls=[]):
-	resp=requests.get(url)
-	soup=bs(resp.text,'html.parser')
+	resp=urlopen(url)
+	soup=bs(resp.read(),"lxml")
 	for link in soup.find_all('a'):
 		temp=link.get('href')
-		domain_len=len(domain)
-		found=1
-		j=0
-		while(found==1 and j<domain_len):
-			if temp is not None:
-				if temp[j]!=domain[j]:
-					found = 0
-				j=j+1
-		if found ==1:
-			urllen=len(allurls)
-			found = 0
-			i=0
-			while(found==0 and i<urllen):
-				if allurls[i]==temp:
-					found=1
-				i=i+1
-			if found==0:
-				allurls.append(temp)
-				outputprint(temp)
-				task(temp,domain,allurls)
-
-
+		if temp is not None:	
+			domain_temp=obtaindomain(temp)
+			if domain_temp==domain:
+				urllen=len(allurls)
+				found = 0
+				i=0
+				while(found==0 and i<urllen):
+					if allurls[i]==temp:
+						found=1
+					i=i+1
+				if found==0:
+					allurls.append(temp)
+					outputprint(temp)
+					task(temp,domain,allurls)
 URl_User="abc"
 outputarr=[]
 URL_User=input('Enter URl : ')
