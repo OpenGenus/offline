c449032f7e1e57dd0036ca0c26994ad72eaf04b7
Tanmay Maheshwari <tanmaymaheshwari1996@gmail.com>
2018-06-04 17:50:12 +0530

    Now this gives Valid JSON
    The error occurred with pages having no meta-description. It's resolved now.

--- json_mapping.py
+++ json_mapping.py
@@ -3,8 +3,9 @@ import bs4
 import requests
 def tojson(fileopen,data):
 	with open(fileopen,'w') as f:
-		f.write("index")
+		f.write("{\"index\":")
 		json.dump(data,f,indent=2)
+		f.write("}")
 filejson='mapping.json'
 index=[]
 file=open('output.txt','r')
@@ -21,10 +22,12 @@ for link in file:
 	data['meta-title']=temp
 	metas=soup.find_all('meta')
 	temp2= ([ meta.attrs['content'] for meta in metas if 'name' in meta.attrs and meta.attrs['name'] == 'description' ])
-	str(temp2)
-	temp3=temp2[0]
-	temp3.strip()
-	data['meta-description']=temp3
+	if(len(temp2) !=0 ):
+		temp3=temp2[0]
+		temp3.strip()
+		data['meta-description']=temp3
+	else:
+		data['meta-description']='no description'
 	index.append(data)
 	i=i+1
 tojson(filejson,index)
