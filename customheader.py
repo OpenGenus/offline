381dab2926e5a136f415c6b34ebc77efe49a8d4b
Tanmay Maheshwari <tanmaymaheshwari1996@gmail.com>
2018-06-23 03:42:02 +0530

    Removed <nav> and included this in head of the html.

--- customheader.py
+++ customheader.py
@@ -4,13 +4,9 @@ def addheader(sp):
 	new_link =  sp.new_tag("link")
 	new_link.attrs["href"] = "custom_opengenus_offline_header_css.css"
 	sp.head.insert(0,new_link)
-	tag=sp.body
-	new_tag=sp.new_tag('div',**{'class':'custom_opengenus_offline_header_class'},id="custom_opengenus_offline_header_div")
-	tag.insert(0,new_tag)
-	tag=sp.find("div",{"id":"custom_opengenus_offline_header_div"})
-	new_tag=sp.new_tag('nav',id="custom_opengenus_offline_header_nav")
-	tag.append(new_tag)
-	tag=sp.find("nav",{"id":"custom_opengenus_offline_header_nav"})
+	nt=sp.new_tag("header",**{'class':'custom_opengenus_offline_header_class'},id="custom_opengenus_offline_header_tagheader")
+	sp.head.insert(1,nt)
+	tag=sp.find("header",{"id":"custom_opengenus_offline_header_tagheader"})
 	new_tag=sp.new_tag('a',href="https://discourse.opengenus.org",id="custom_opengenus_offline_header_a1")
 	tag.append(new_tag)
 	new_tag=sp.new_tag('img',src="https://discourse-cdn-sjc2.com/standard17/uploads/opengenus/original/1X/5c936e504d024c2da59395fe5a9ca54241106f1b.png", alt="OpenGenus",height="50", width="205",id="custom_opengenus_offline_header_i1")
